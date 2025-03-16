ba9c75579a5c1d3979ae749478f125aca54ca089
Patrick McGreal <patrick.mcgreal.dev@gmail.com>
2025-03-16 21:32:44 +0100

    display default midsummer countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 
 @Component({
@@ -10,4 +10,30 @@ import { RouterOutlet } from '@angular/router'
 })
 export class AppComponent {
   title = 'FrontendChallenge'
+
+  @Input() eventTitle: string = 'Midsummer Eve'
+  @Input() eventDate: Date = new Date(2025, 6, 21)
+
+  deltaTime = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+
+  ngOnInit() {
+    // https://stackoverflow.com/questions/13903897/javascript-return-number-of-days-hours-minutes-seconds-between-two-dates
+
+    let deltaSeconds = Math.abs(this.eventDate.getTime() - new Date().getTime()) / 1000;
+
+    // calculate whole days, removing the remainder
+    this.deltaTime.days = Math.floor(deltaSeconds / 86400);
+    deltaSeconds -= this.deltaTime.days * 86400;
+
+    // calculate whole hours, removing the remainder
+    this.deltaTime.hours = Math.floor(deltaSeconds / 3600) % 24;
+    deltaSeconds -= this.deltaTime.hours * 3600;
+
+    // calculate whole minutes, removing the remainder
+    this.deltaTime.minutes = Math.floor(deltaSeconds / 60) % 60;
+    deltaSeconds -= this.deltaTime.minutes * 60;
+
+    // remaining time in seconds
+    this.deltaTime.seconds = Math.floor(deltaSeconds);
+  }
 }
