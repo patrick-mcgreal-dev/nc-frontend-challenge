bd8c48ff2712e02c78c50dedfb2d9d44ccd7556a
Patrick McGreal <patrick.mcgreal.dev@gmail.com>
2025-03-16 21:32:44 +0100

    prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,34 +14,34 @@ export class AppComponent {
   @Input() eventTitle: string = 'Midsummer Eve'
   @Input() eventDate: Date = new Date(2025, 6, 21)
 
-  deltaTime = { days: 0, hours: 0, minutes: 0, seconds: 0 };
-  deltaIntervalId: any;
+  deltaTime = { days: 0, hours: 0, minutes: 0, seconds: 0 }
+  deltaIntervalId: any
 
   ngOnInit() {
-    this.updateDeltaTime();
+    this.updateDeltaTime()
     this.deltaIntervalId = setInterval(() => {
-      this.updateDeltaTime();
-    }, 500);
+      this.updateDeltaTime()
+    }, 500)
   }
 
   updateDeltaTime() {
     // https://stackoverflow.com/questions/13903897/javascript-return-number-of-days-hours-minutes-seconds-between-two-dates
 
-    let deltaSeconds = Math.abs(this.eventDate.getTime() - new Date().getTime()) / 1000;
+    let deltaSeconds = Math.abs(this.eventDate.getTime() - new Date().getTime()) / 1000
 
     // calculate whole days, removing the remainder
-    this.deltaTime.days = Math.floor(deltaSeconds / 86400);
-    deltaSeconds -= this.deltaTime.days * 86400;
+    this.deltaTime.days = Math.floor(deltaSeconds / 86400)
+    deltaSeconds -= this.deltaTime.days * 86400
 
     // calculate whole hours, removing the remainder
-    this.deltaTime.hours = Math.floor(deltaSeconds / 3600) % 24;
-    deltaSeconds -= this.deltaTime.hours * 3600;
+    this.deltaTime.hours = Math.floor(deltaSeconds / 3600) % 24
+    deltaSeconds -= this.deltaTime.hours * 3600
 
     // calculate whole minutes, removing the remainder
-    this.deltaTime.minutes = Math.floor(deltaSeconds / 60) % 60;
-    deltaSeconds -= this.deltaTime.minutes * 60;
+    this.deltaTime.minutes = Math.floor(deltaSeconds / 60) % 60
+    deltaSeconds -= this.deltaTime.minutes * 60
 
     // remaining time in seconds
-    this.deltaTime.seconds = Math.floor(deltaSeconds);
+    this.deltaTime.seconds = Math.floor(deltaSeconds)
   }
 }
